ed19d3d15aabaa1930f6b336d2c012528147175e
Duy Hoang <hoangducduybh2002@gmail.com>
2025-04-10 14:27:22 +0700

    Initial spring boot project

---
+++ src/main/java/nashtech/rookie/uniform/UniformApplication.java
@@ -0,0 +1,13 @@
+package nashtech.rookie.uniform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nashtech/rookie/uniform/UniformApplicationTests.java
@@ -0,0 +1,13 @@
+package nashtech.rookie.uniform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
