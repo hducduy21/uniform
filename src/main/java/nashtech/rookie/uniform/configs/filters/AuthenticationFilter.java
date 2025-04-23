6ddcab6e3ed5a59b40b8c5467745c8ff5ecb13b8
Hoàng Đức Duy <hoangducduybh2002@gmail.com>
2025-04-23 08:25:47 +0700

    feature: add AOP-based logging for layers

--- src/main/java/nashtech/rookie/uniform/configs/filters/AuthenticationFilter.java
+++ src/main/java/nashtech/rookie/uniform/configs/filters/AuthenticationFilter.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import nashtech.rookie.uniform.configs.security.CustomUserDetails;
 import nashtech.rookie.uniform.configs.security.JwtUtil;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
@@ -29,22 +31,35 @@ public class AuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        //get token from header
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !StringUtils.startsWith(authHeader,"Bearer ")) {
+        try {
+            //create requestId and put it to MDC
+            String requestId = UUID.randomUUID().toString();
+            MDC.put("requestID", requestId);
+
+            //get token from header
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader == null || !StringUtils.startsWith(authHeader,"Bearer ")) {
+                MDC.put("userId", "unverified");
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = StringUtils.substring(authHeader, 7);
+
+            //extract phoneNumber to identify user and push it to context holder
+            String phoneNumber = jwtUtil.extractClaim(token, "phoneNumber");
+            if(phoneNumber != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                String userId = jwtUtil.extractClaim(token, "userId");
+                MDC.put("userId", userId);
+
+                CustomUserDetails userDetails = (CustomUserDetails) userDetailsService.loadUserByUsername(phoneNumber);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
             filterChain.doFilter(request, response);
-            return;
         }
-        String token = StringUtils.substring(authHeader, 7);
-
-        //extract phoneNumber to identify user and push it to context holder
-        String phoneNumber = jwtUtil.extractClaim(token, "phoneNumber");
-        if(phoneNumber != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            CustomUserDetails userDetails = (CustomUserDetails) userDetailsService.loadUserByUsername(phoneNumber);
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        finally {
+            MDC.clear();
         }
-
-        filterChain.doFilter(request, response);
     }
 }
